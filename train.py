2a0aa3156977553fd60fde12cfa16b21197f51c3
Giuseppe <ipeppegili@gmail.com>
2021-08-05 20:28:49 +0200

    Update train.py

--- train.py
+++ train.py
@@ -68,7 +68,7 @@ ds = TabularDatasetFactory.from_delimited_files(path=data_path)
 x, y = clean_data(ds)
 
 # Split data into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 if __name__ == '__main__':
     main()
