da31796d2310bfdbccd3d462ad90268813bad6a4
Giuseppe <ipeppegili@gmail.com>
2021-08-05 11:17:04 +0200

    bug fixing

--- train.py
+++ train.py
@@ -10,21 +10,9 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-data_path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-ds = TabularDatasetFactory.from_delimited_files(path=data_path)
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
-
 run = Run.get_context()
 
+# Transform and clean data function
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
@@ -52,8 +40,8 @@ def clean_data(data):
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     
     return x_df, y_df
-    
 
+# Main function
 def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
@@ -66,10 +54,21 @@ def main():
     run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
 
-    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(X_train, y_train)
 
-    accuracy = model.score(x_test, y_test)
+    accuracy = model.score(X_test, y_test)
     run.log("Accuracy", np.float(accuracy))
+
+# Create TabularDataset using TabularDatasetFactory
+# Data is located at: "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+data_path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+ds = TabularDatasetFactory.from_delimited_files(path=data_path)
+
+# Clean data
+x, y = clean_data(ds)
+
+# Split data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
 
 if __name__ == '__main__':
     main()
